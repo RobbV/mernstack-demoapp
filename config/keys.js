7036e73f56b80d7ea1bc2d0adbbfa7b303b0902e
Robbert Van der Grift <rob.vdg.14@gmail.com>
2018-12-29 15:56:25 -0500

    server and some routes setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mogoURI: "mongodb://robbert:robbert12@ds141623.mlab.com:41623/devconnector12"
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+// required packages
+const express = require("express");
+const mongoose = require("mongoose");
+
+//Route files
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//db config
+const db = require("./config/keys").mogoURI;
+
+// connect  to mogoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("hello"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server runnig on ${port}`));
